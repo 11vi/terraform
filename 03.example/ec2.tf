31a9421b604ffde72deb05c35f9969a84fdf8a9e
vikram <vikram.y24@gmail.com>
2021-07-28 14:34:52 +0530

    ec2

--- 03.example/ec2.tf
+++ 03.example/ec2.tf
@@ -25,7 +25,7 @@ resource "aws_security_group" "allow_ssh" {
     from_port            = 0
     to_port              = 0
     protocol             = "-1"
-    cidr_blocks          = ["0.0.0.0/0"]
+    cidr_blocks          = [0.0.0.0/0]
 
   }
 
