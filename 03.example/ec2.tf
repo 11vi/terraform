686a9cc6ebaa5b45c8f666fceb4dad56ddb0fe95
vikram <vikram.y24@gmail.com>
2021-07-29 10:25:59 +0530

    ec2

--- 03.example/ec2.tf
+++ 03.example/ec2.tf
@@ -1,41 +1,42 @@
-resource "aws_instance" "samplr" {
-  ami                      = "ami-074df373d6bafa625)"
-  instance_type            = "t2.micro"
-  vpc_security_group_ids   = []
+resource "aws_instance" "sample" {
+  ami                   =
+  instance_type         = "t3.micro"
+  vpc_security_group_ids = []
 
-  tags = {
-    Name                  = "sample"
+  tags                  = {
+    Name                = "Sample"
   }
-}
+
 
 resource "aws_security_group" "allow_ssh" {
-  name                    = "allow_ssh"
-  description             = "Allow ssh"
-
+  name                  = "allow_ssh"
+  description           = "allow_ssh"
 
   ingress {
-    description          = "ssh"
-    from_port            = 22
-    to_port              = 22
-    protocol             = "tcp"
-    cidr_blocks          = [0.0.0.0/0]
+    description         = "SSH"
+    from_port           = 22
+    to_port             = 22
+    protocol            = "tcp"
+    cidr_blocks         = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port            = 0
-    to_port              = 0
-    protocol             = "-1"
-    cidr_blocks          = [0.0.0.0/0]
-
+    from_port           = 0
+    to_port             = 0
+    protocol            = "-1"
+    cidr_blocks         = ["0.0.0.0/0"]
   }
 
-  tags = {
-    Name                = "allow_tls"
+  tags                  = {
+    Name                = "allow_ssh"
   }
 }
-output "sg-attributes" {
-  value = "aws_security group.all_ssh"
+
+output "ec2-attributes" {
+  value = aws_instance.sample
 }
+
+
 provider "aws" {
-  region = "ua-east-1"
+region = "us-east-1"
 }
